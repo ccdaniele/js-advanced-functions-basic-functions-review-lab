6fb6d28daba266723ca80d054f41b5d573d51d8c
Daniel <danieleduardocald@gmail.com>
2021-02-16 10:09:22 -0500

    commit tree 9d002b2b653f3520d85f28defe255d359e5b8c3c

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Your code here
+
+function saturdayFun(activity='roller-skate' ){
+
+     return `This Saturday, I want to ${activity}!`
+
+};
+
+
+function mondayWork(activity="go to the office" ){
+
+    return `This Monday, I will ${activity}.`
+
+};
+
+function wrapAdjective(main="*"){
+        `You are a ${main} dedicated programmer ${main}`
+
+    return function(parameter='special'){
+        return `You are ${parameter}`
+    };
+};
+
